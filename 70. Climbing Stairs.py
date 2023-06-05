92ae207166a5722661f6b21b1ca479e4db810a66
Girish Jha <77683275+DarkShadowKonoha@users.noreply.github.com>
2023-06-05 23:23:58 +0530

    Update 70. Climbing Stairs.py
    Using DP, instead of using an extra array, only using two extra variables to store results

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,6 +1,16 @@
-class Solution:
-    def climbStairs(self, n):
-        f1, f2 = 1, 2
+# class Solution:
+#     def climbStairs(self, n):
+#         f1, f2 = 1, 2
+#         for i in range(n-1):
+#             f1, f2 = f2, f1+f2
+#         return f1
+
+    class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
         for i in range(n-1):
-            f1, f2 = f2, f1+f2
-        return f1
+            tmp = one
+            one = one + two
+            two = tmp
+
+        return one
