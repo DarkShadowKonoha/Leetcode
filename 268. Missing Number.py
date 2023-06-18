e5810afd8573c3c69fa5e6b8156ff204c393e9a3
Girish Jha <77683275+DarkShadowKonoha@users.noreply.github.com>
2023-06-18 23:08:42 +0530

    Update 268. Missing Number.py
    optimized the code

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,7 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)+1):
-            if i in nums:
-                continue
-            else:
-                return i
+        # for i in range(len(nums)+1):
+        #     if i not in nums:
+        #         return i
+
+        # taking the difference of sum of range and sum of nums array, the result should be the missing value
+        return sum(range(len(nums)+1)) - sum(nums)
